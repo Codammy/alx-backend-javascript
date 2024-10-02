fe7eee1d86745226989e3ac4f69f385d5341fc77
Damilola <damisco005@gmail.com>
2024-10-02 11:13:51 +0100

    0x00-ES6_basic

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,6 +1,6 @@
 export default function iterateThroughObject(reportWithIterator) {
   let str = '';
-  const iteratorLength = str(reportWithIterator.length - 1);
+  const iteratorLength = (reportWithIterator.length - 1).toString();
   for (const idx in reportWithIterator) {
     if (idx) {
       str += (idx === iteratorLength) ? reportWithIterator[idx] : (`${reportWithIterator[idx]} | `);
