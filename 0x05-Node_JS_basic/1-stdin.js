2bcf2851422f1669a0aaabcca4c3caf71a8672ba
codam <damisco005@gmail.com>
2024-11-27 05:31:54 +0100

    new commit

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,8 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
-  const name = data.toString().trim('\n');
-  console.log(`Your name is: ${name}`);
-  process.exit();
+  process.stdout.write(`Your name is: ${data}`);
+  process.stdin.pause();
 });
-process.on('exit', (code) => {
-  console.log('This important software is now closing');
-  process.exit(code);
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
