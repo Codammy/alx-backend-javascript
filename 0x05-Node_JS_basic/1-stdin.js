452edc708bcbe10c07b7cb70e203cee5ad4a3560
codam <damisco005@gmail.com>
2024-11-27 11:39:55 +0100

    new commit

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,10 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+console.log('Welcome to Holberton School, what is your name?');
+
 process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data}`);
   process.stdin.pause();
 });
+
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,5 +1,37 @@
-function countStudents(path) {
-    
+const fs = require('fs');
+
+function csvToObj(data, header) {
+  return data.map((std) => {
+    const obj = {};
+    const tmp = std.split(',');
+    let i = 0;
+    header.forEach((h) => {
+      obj[h] = tmp[i];
+      i += 1;
+    });
+    return obj;
+  });
 }
 
+function countStudents(path) {
+  let students = [];
+  let header;
+  try {
+    let data = fs.readFileSync(path, 'utf8').trim('\n');
+    data = data.split('\n');
+    header = data[0].split(',');
+    students = csvToObj(data.splice(1), header);
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+
+  let fields = students.map((student) => student[header[header.length - 1]]);
+  console.log(fields);
+  console.log(`Number of students: ${students.length}`);
+  //   students.for;
+
+  console.log(students);
+}
+
+countStudents('./database.csv');
 module.exports = countStudents;
