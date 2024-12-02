a65f9846593510207db829259dcfe497e4dbe4d7
codam <damisco005@gmail.com>
2024-12-02 09:41:43 +0100

    trying hard to meet checker

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,9 +1,14 @@
 /* eslint-disable jest/expect-expect */
 const assert = require('assert');
-const calculateNumber = require('./0-calcul.js');
+const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', function () {
   it('returns the sum of a, b to the nearest whole number', function () {
-    assert(calculateNumber(1.5, 3.7) === 6);
+    let res = calculateNumber(1.5, 3.7);
+    assert.strictEqual(res, 6);
+    res = calculateNumber(1.5, 1.5);
+    assert.strictEqual(res, 4);
+    res = calculateNumber(2, 1);
+    assert.strictEqual(res, 3);
   });
 });
