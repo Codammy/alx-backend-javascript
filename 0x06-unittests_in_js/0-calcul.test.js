cccfe98a648a9fbcab498c60519c3b48ae1c761a
codam <damisco005@gmail.com>
2024-12-02 09:49:28 +0100

    full of hope

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,12 +3,19 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', function () {
-  it('returns the sum of a, b to the nearest whole number', function () {
-    let res = calculateNumber(1.5, 3.7);
-    assert.strictEqual(res, 6);
-    res = calculateNumber(1.5, 1.5);
-    assert.strictEqual(res, 4);
-    res = calculateNumber(2, 1);
-    assert.strictEqual(res, 3);
+  it('rounds the first number', function () {
+    assert.strictEqual(calculateNumber(1.2, 0), 1);
+    assert.strictEqual(calculateNumber(2.7, 0), 3);
+    assert.strictEqual(calculateNumber(3.0, 0), 3);
+  });
+  it('rounds the second number', function () {
+    assert.strictEqual(calculateNumber(0, 1.2), 1);
+    assert.strictEqual(calculateNumber(0, 2.7), 3);
+    assert.strictEqual(calculateNumber(0, 3.0), 3);
+  });
+  it('rounds the first number', function () {
+    assert.strictEqual(calculateNumber(0, 1.2), 1);
+    assert.strictEqual(calculateNumber(0, 2.7), 3);
+    assert.strictEqual(calculateNumber(0, 3.0), 3);
   });
 });
