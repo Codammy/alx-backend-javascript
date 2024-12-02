8828f531cf4dabe655bc5eec23dfe25b93f7bd8f
codam <damisco005@gmail.com>
2024-12-02 09:13:22 +0100

    mocha test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,5 +1,6 @@
-import { assert } from 'chai';
-import { calculateNumber } from './0-calcul.js';
+/* eslint-disable jest/expect-expect */
+const assert = require('assert');
+const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', function () {
   it('returns the sum of a, b to the nearest whole number', function () {
