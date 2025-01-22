86e098890ac2c6a1729049c322fd51714ea3af75
Mr Dami <damisco005@gmail.com>
2025-01-22 11:18:54 +0100

    new commit

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,50 +1,33 @@
-const http = require('http');
-const { exec } = require('child_process');
+const http = require("http");
+const { exec } = require("child_process");
 
-const app = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello ALX!');
-  } else if (req.url === '/students') {
-    const database = process.argv[2];
 
-    if (!database) {
-      res.writeHead(500, { 'Content-Type': 'text/plain' });
-      res.end('Database file not provided');
-      return;
+
+const app = http
+  .createServer(async (req, res) => {
+    console.log(req.url);
+    switch (req.url) {
+      case "/":
+        res.write("Hello ALX!");
+        break;
+      case "/students":
+        res.write("This is the list of our students\n");
+        const ch = exec(`node 3-read_file_async.js ${process.argv[2]}`);
+
+        ch.stderr.on("data", (err) => {
+          info = err.toString().split("\n")[4].split(": ")[1];
+        });
+        ch.stdout.on("data", (data) => {
+          info = data.toString();
+        });
+        res.write(info);
+        break;
+      default:
+        break;
     }
 
-    const child = exec(`node 3-read_file_async.js ${database}`);
-
-    let output = '';
-    let errorOccurred = false;
-
-    child.stdout.on('data', (data) => {
-      output += data;
-    });
-
-    child.stderr.on('data', (error) => {
-      errorOccurred = true;
-      output += error; 
-    });
-
-    child.on('close', (code) => {
-      if (errorOccurred || code !== 0) {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Error occurred while processing the database\n' + output);
-      } else {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end(`This is the list of our students\n${output}`);
-      }
-    });
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found');
-  }
-});
-
-app.listen(1245, () => {
-  console.log('Server is listening on port 1245');
-});
+    res.end();
+  })
+  .listen(1245);
 
 module.exports = app;
